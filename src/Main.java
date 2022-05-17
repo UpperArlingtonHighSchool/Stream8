3f68bd3123b5da73189b09103d8522136e9d5cac
Zhenyang Feng <feng.6@C02NX1LDG083.local>
2022-05-16 22:15:39 -0400

    initial code

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String args[]){
+        StreamStudyRunner run = new StreamStudyRunner();
+        run.initiate();
+    }
+}
---
+++ src/StreamStudyRunner.java
@@ -0,0 +1,5 @@
+public class StreamStudyRunner {
+    public void initiate(){
+
+    }
+}
